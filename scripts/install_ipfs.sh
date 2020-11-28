566a80bb5c2b0345d05bde5e55297227b8dab39c
Alexis <rldev25@gmail.com>
2020-11-28 10:40:27 -0500

    up

--- scripts/install_ipfs.sh
+++ scripts/install_ipfs.sh
@@ -23,7 +23,7 @@ ipfs init
 ipfs config profile apply server
 #ipfs config profile apply randomports
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-#ipfs config --json Addresses.Swarm '[]'
+ipfs config --json Addresses.Swarm '[]'
 ipfs config --json Gateway.NoFetch true
 
 # enable and start service ipfs
